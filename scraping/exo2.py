22c1bcc6c379fb07c07775036bd97a8d5816c47e
123justin123 <justinboisson@gmail.com>
2023-10-25 11:22:24 +0200

    Ex 2: Écriture dans result.csv des équipes avec un différentiel positif (victoires - défaites) depuis les 10 premières pages

--- scraping/exo2.py
+++ scraping/exo2.py
@@ -1,20 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-for page_num in range(1, 4):
-    url = f'https://www.scrapethissite.com/pages/forms/?page_num={page_num}'
-    response = requests.get(url)
+with open('result.csv', 'w', newline='') as csv_file:
+    fieldnames = ['Equipe', 'Victoire', 'Défaite']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    for page_num in range(1, 11):
+        url = f'https://www.scrapethissite.com/pages/forms/?page_num={page_num}'
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        table = soup.find('table', {'class': 'table'})
-        rows = table.find_all('tr')
-        for row in rows:
-            cells = row.find_all('td')
-            if len(cells) > 0:
-                equipe = cells[0].text.strip()
-                annee = cells[1].text.strip()
-                pourcentage = cells[5].text.strip()
-                print(f'{equipe} - {annee} - {pourcentage} %')
-    else:
-        print(f'La requête pour la page {page_num} a échoué avec le code d\'état {response.status_code}')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            table = soup.find('table', {'class': 'table'})
+            rows = table.find_all('tr')
+            for row in rows:
+                cells = row.find_all('td')
+                if len(cells) > 0:
+                    equipe = cells[0].text.strip()
+                    victoire = cells[2].text.strip()
+                    defaite = cells[3].text.strip()
+                    diff = int(victoire) - int(defaite)
+                    if diff > 0:
+                        writer.writerow({'Equipe': equipe, 'Victoire': victoire, 'Défaite': defaite})
+        else:
+            print(f'La requête pour la page {page_num} a échoué avec le code d\'état {response.status_code}')
